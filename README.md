b085a85eea6ab4268536e022fe6587e656d3abc5
Igor <siafin2010@gmail.com>
2020-05-31 22:51:27 +0300

    test deploy

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+SkillUp training project "films app"
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ build/precache-manifest.eb16527f949a96c9e14621ae9f05b15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea60a02ea955127361359cdd207565c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f02e154165bd29007bf",
+    "url": "/static/css/main.435d2f3e.chunk.css"
+  },
+  {
+    "revision": "4cc6a46e5d84f53f9598",
+    "url": "/static/js/2.7ec8772f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7ec8772f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f02e154165bd29007bf",
+    "url": "/static/js/main.96b047eb.chunk.js"
+  },
+  {
+    "revision": "fe04bf8795fd3fa5fd8e",
+    "url": "/static/js/runtime-main.d07f3449.js"
+  }
+]);
